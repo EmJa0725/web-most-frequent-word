8d436c209117a02ac3a222533db851d2a1416e8b
Julian Andres Mendez Romero <julian.mendez@grupomok.com>
2022-01-16 21:41:39 -0500

    Changes on request body having array of ignored words selected by the user

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import cross_origin
 import requests
 from bs4 import BeautifulSoup
 from collections import Counter
@@ -7,13 +8,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Bienvenido a la api"
+    response = jsonify(message="Server is running")
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
 
 @app.route('/frequent_word', methods=['POST'])
+@cross_origin()
 def frequent_word_ul():
     if request.method == 'POST':  
         body = request.get_json() 
-        text = parse_url(body['url'])
+        text = parse_url(body['url'], body['ignored'])
         top_words = create_dictionary(text, body['top'])  
     
     return jsonify({
@@ -21,25 +25,28 @@ def frequent_word_ul():
         }), 201
     
 # Views
-def parse_url(url):
+def parse_url(url, ignored):
     """Extract data from url and parse each word into a list"""  
     filtered_words = []
     symbols = "!@#$%^&*()_-+={[}]|\;:\"<>?/.,"
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) '\
+            'AppleWebKit/537.36 (KHTML, like Gecko) '\
+            'Chrome/75.0.3770.80 Safari/537.36'}
     
-    source_text = requests.get(url).text
+    source_text = requests.get(url, headers=headers).content
     soup = BeautifulSoup(source_text, 'html.parser')
     
     # get all content without html tags from page
     text_content = soup.find('body').text
-    #print(text_content)
     # Break test into word and convert them into lower case
     words = text_content.lower().split()
-    
+    print(ignored)
     # Filter valid words removing numbers and special characters
     for word in words:
         for  symbol in symbols : 
             word = word.replace(symbol, '')
-        if len(word) and not word.isnumeric():
+        if len(word) and not word.isnumeric() and word not in ignored:
+            if word in ignored: continue
             filtered_words.append(word)
             
     return filtered_words
--- boot_strap.sh
+++ boot_strap.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-export FLASK_APP=./app/index.py
+export FLASK_APP=./app/app.py
 flask run -h 0.0.0.0
